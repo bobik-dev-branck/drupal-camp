13f94b45aed6cb0ab00d5d044ed90697b1f92377
bobik-dev-branck <b.melnychuk@dev-branch.com>
2023-01-11 18:10:20 +0200

    5843.2.dev Created a management form to store the URL

---
+++ web/modules/custom/exchange_rates/src/Form/ExchangeRatesSettingsForm.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\exchange_rates\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configure Exchange rates settings for this site.
+ */
+class ExchangeRatesSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'exchange_rates_exchange_rates';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['exchange_rates.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['show_block'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Do you want to show block'),
+      '#default_value' => $this->config('exchange_rates.settings')->get('show_block'),
+    ];
+
+    $form['url'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Exchange rates API'),
+      '#description' => 'Example - https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=',
+      '#default_value' => $this->config('exchange_rates.settings')->get('url'),
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if ($form_state->getValue('url') != 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=') {
+      $form_state->setErrorByName('url', $this->t('The value is not correct.'));
+    }
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('exchange_rates.settings')
+      ->set('show_block', $form_state->getValue('show_block'))
+      ->set('url', $form_state->getValue('url'))
+      ->save();
+    parent::submitForm($form, $form_state);
+  }
+
+}
