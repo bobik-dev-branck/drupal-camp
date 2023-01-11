cfbf816739f2a0da1dcc327ec0069d5beffc0dae
bobik-dev-branck <b.melnychuk@dev-branch.com>
2023-01-11 16:30:27 +0200

    5843. Created the block to show Exchange rates

---
+++ web/modules/custom/exchange_rates/src/Plugin/Block/ExchangeRatesBlock.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\exchange_rates\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides data about currency exchange rates.
+ *
+ * @Block(
+ *   id = "exchange_rates",
+ *   admin_label = @Translation("Exchange_rates"),
+ * )
+ */
+class ExchangeRatesBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $httpClient = \Drupal::httpClient();
+    $date = date("Ymd");
+    $exchangeRates =
+      json_decode($httpClient
+        ->get('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=' . $date .'&json')
+        ->getBody());
+
+    foreach ($exchangeRates as $exchangeRate) {
+      $data[$exchangeRate->cc] = $exchangeRate->rate;
+    }
+
+    $renderable = [
+      '#theme' => 'block_exchange_rates',
+      '#data' => $data,
+    ];
+
+    return $renderable;
+
+  }
+}
