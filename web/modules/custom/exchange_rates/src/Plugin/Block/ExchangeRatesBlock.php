dae48d7622d5e95a679873d88f778a44d47230f1
bobik-dev-branck <b.melnychuk@dev-branch.com>
2023-01-12 17:55:03 +0200

    5843.4 Implemented Dependency Injection for block and replace old code with services methods.

--- web/modules/custom/exchange_rates/src/Plugin/Block/ExchangeRatesBlock.php
+++ web/modules/custom/exchange_rates/src/Plugin/Block/ExchangeRatesBlock.php
@@ -3,6 +3,9 @@
 namespace Drupal\exchange_rates\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\exchange_rates\ExchangeRatesService;
+use Psr\Container\ContainerInterface;
 
 /**
  * Provides data about currency exchange rates.
@@ -12,29 +15,77 @@ use Drupal\Core\Block\BlockBase;
  *   admin_label = @Translation("Exchange_rates"),
  * )
  */
-class ExchangeRatesBlock extends BlockBase {
+class ExchangeRatesBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Constructor.
+   *
+   * @param array $configuration
+   *   Configuration array.
+   * @param string $plugin_id
+   *   Plugin ID.
+   * @param mixed $plugin_definition
+   *   Plugin definition.
+   * @param \Drupal\exchange_rates\ExchangeRatesService $exchange_rates
+   *   The Exchange Rates Service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ExchangeRatesService $exchange_rates) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->exchangeRates = $exchange_rates;
+  }
+
+  /**
+   * Create method.
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   Container.
+   * @param array $configuration
+   *   Configuration array.
+   * @param string $plugin_id
+   *   Plugin ID.
+   * @param mixed $plugin_definition
+   *   Plugin definition.
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('exchange_rates.service'),
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $httpClient = \Drupal::httpClient();
-    $date = date("Ymd");
-    $exchangeRates =
-      json_decode($httpClient
-        ->get('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=' . $date . '&json')
-        ->getBody());
-
-    foreach ($exchangeRates as $exchangeRate) {
-      $data[$exchangeRate->cc] = $exchangeRate->rate;
+    $showBlock = $this->exchangeRates->getConfig('show_block');
+    if ($showBlock == FALSE) {
+      return;
     }
 
-    $renderable = [
-      '#theme' => 'block_exchange_rates',
-      '#data' => $data,
-    ];
+    if ($showBlock == TRUE) {
+      $url = $this->exchangeRates->getConfig('url');
 
-    return $renderable;
+      if ($url) {
+        $data = $this->exchangeRates->getExchangeRates($url);
+
+        if (!empty($data)) {
+          $renderable = [
+            '#theme' => 'block_exchange_rates',
+            '#data' => $data,
+          ];
+
+          return $renderable;
+        }
+
+        return;
+      }
+
+      return;
+    }
+
+    return;
 
   }
 
